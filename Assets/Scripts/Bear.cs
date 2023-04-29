034e304d778106b5a7690c262c563a9399542301
Matt Lee <mdlee.md@gmail.com>
2023-04-29 12:04:31 -0500

    Added basic bear animations and bear controller script

---
+++ Assets/Scripts/Bear.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bear : MonoBehaviour
+{
+    [field:SerializeField] public UnityEvent OnLookAtClipboard { get; private set; }
+
+    [field:SerializeField] public UnityEvent OnIdle { get; private set; }
+
+    [SerializeField] float lookAtClipboardDelay;
+
+    Animator animator;
+
+
+
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();        
+    }
+
+    
+
+    public void LookAtClipboard(float delay)
+    {
+        StartCoroutine(PlayAnimation("LookAtClipboard", delay, lookAtClipboardDelay, OnLookAtClipboard));
+    }
+
+    public void Idle(float delay)
+    {
+        StartCoroutine(PlayAnimation("Idle", delay, 0, OnIdle));
+    }
+
+    IEnumerator PlayAnimation(string animationName, float delay, float duration, UnityEvent eventToInvoke)
+    {
+        if (delay > 0)
+            yield return new WaitForSeconds(delay);
+
+        animator.Play(animationName);
+
+        if (duration > 0)
+            yield return new WaitForSeconds(duration);
+
+        eventToInvoke?.Invoke();
+    }
+}

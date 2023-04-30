c48c9ae83ce2c0cf985629529c88b0cb01dd8182
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-04-30 19:01:54 -0400

    Get rid of debug key for attention staff.

--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -55,7 +55,6 @@ public class SongManager : MonoBehaviour
     {
         if (InputSystem.InputActions.Debug.DebugSound.WasPerformedThisFrame())
         {
-            PlayAttentionStaff();
         }
     }
 

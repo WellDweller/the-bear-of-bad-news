3bc20a40bcede6ab25e8c4c9a3e46270c23237b8
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2023-04-29 23:10:43 -0400

    Add delete wav files script.

---
+++ delete_wav_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Usage: ./delete_wav_files.sh [dry-run]
+
+DRY_RUN=false
+if [ "$1" == "dry-run" ]; then
+  DRY_RUN=true
+fi
+
+find_files() {
+  find . -type f \( -iname "*.wav" -o -iname "*.wav.meta" \)
+}
+
+if $DRY_RUN; then
+  echo "Dry run mode. The following files would be deleted:"
+  find_files
+else
+  echo "Deleting files..."
+  find_files -exec rm -v {} \;
+  echo "Deletion complete."
+fi
